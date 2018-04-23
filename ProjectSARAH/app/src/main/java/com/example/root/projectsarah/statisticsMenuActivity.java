4250e3e2e49b4225845b1bd5869f555f3c24533c
Adi <crocplays26@gmail.com>
2018-04-23 18:56:11 +0300

    integrated GraphView for statistic usage

--- ProjectSARAH/app/src/main/java/com/example/root/projectsarah/statisticsMenuActivity.java
+++ ProjectSARAH/app/src/main/java/com/example/root/projectsarah/statisticsMenuActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 
 public class statisticsMenuActivity extends AppCompatActivity {
 
@@ -16,6 +20,18 @@ public class statisticsMenuActivity extends AppCompatActivity {
         toolbar.setTitle("Statistics");
         setSupportActionBar(toolbar);
 
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -24,5 +40,6 @@ public class statisticsMenuActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),MainMenu.class));
             }
         });
+
     }
 }
