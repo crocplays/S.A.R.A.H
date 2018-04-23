695d58810700b2d7d1e90855a758b97d6b19dd99
Adi <crocplays26@gmail.com>
2018-04-23 11:12:08 +0300

    ADDED ANDROID APP

---
+++ ProjectSARAH/app/src/main/java/com/example/root/projectsarah/settingsMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.root.projectsarah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class settingsMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_menu);
+    }
+}
